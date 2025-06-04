3dc0637fb63f8f73dfc6bbbcc203595a930ca080
Danylo Sydorenko <logerk15a@gmail.com>
2025-06-04 12:19:35 +0200

    test: verify navigation component renders

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('store-admin-panel');
   });
 
-  it('should render title', () => {
+  it('should render navigation component', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('store-admin-panel app is running!');
+    expect(compiled.querySelector('app-nav')).toBeTruthy();
   });
 });
